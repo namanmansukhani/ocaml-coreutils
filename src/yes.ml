7e200ab6d53da4c8f637f4e87bdccd3be1d90351
Naman Mansukhani <naman.mansukh@gmail.com>
2022-07-25 19:11:02 +0530

    Added yes utility

---
+++ src/yes.ml
@@ -0,0 +1,23 @@
+let program_name = "yes"
+(* Still need to implement flags for physical and logical addresses *)
+let usage_msg = "usage: yes [args ...]"
+let args = ref []
+let anon_fun arg = args := arg :: !args
+let speclist = []
+let exit_code = ref 0
+
+let yes args = 
+  let word = (match args with | [] -> "y" | (x::xs) -> x) in
+    while true do 
+      try
+        Printf.printf "%s\n" word
+      with
+        Sys_error (err_msg) -> (Printf.printf "%s: %s\n" program_name err_msg;exit_code:=1)
+    done
+
+let main () =
+  Arg.parse speclist anon_fun usage_msg;
+  yes (List.rev !args);
+  exit (!exit_code)
+
+let _ = main ()
